975cdeb6002313f996aed38261a1ee770c3be7f5
Fernando Arriagada <103617720+FernandoLabarca@users.noreply.github.com>
2025-09-23 12:17:21 -0300

    Update ItemsMenu.jsx
    Actualizacion de las imagens del menu

--- Proyecto04-Restaurant/src/Components/ItemsMenu.jsx
+++ Proyecto04-Restaurant/src/Components/ItemsMenu.jsx
@@ -1,39 +1,46 @@
+import porkShop from '../assets/hamburguesa_the_pork_shop_1673959505pmHdnV.jpeg';
+import spicyPulledPork from '../assets/hamburguesa_spicy_pulled_pork_1673960044ZD23BB.jpeg';
+import americanStylePork from '../assets/hamburguesa_american_style_pork_1673959958DGfHlq.jpeg';
+import buffaloChicken from '../assets/buffalo_chicken_16655253277z965V.jpg';
+import spicyBuffaloChicken from '../assets/spicy_buffalo_chicken_1665528552nAEXPA.jpg';
+import americanStyleChicken from '../assets/american_style_chicken_166552695897Ngbm.jpg';
+
 export const Hamburguesa = [
     {
         nombre: "The Pork Shop",
         detalle: "Jugoso Pulled Pork, bañado en queso cheddar fundido, cremoso coleslaw, pepinillos dulces y nuestra Secret Sauce. Incluye una salsa a elección y una porción de papas fritas crinkle!",
         precio: "5500",
-        imgUrl: "./src/Assets/hamburguesa_the_pork_shop_1673959505pmHdnV.jpeg"
+        imgUrl: porkShop
     },
     {
         nombre: "Spicy Pulled Pork",
         detalle: "Jugoso Pulled Pork bañado en salsa buffalo (picor medio/alto) , cremoso coleslaw, pepinillos dulces y nuestra Ali Oli. Incluye una salsa a elección y una porción de papas fritas crinkle!",
         precio: "5500",
-        imgUrl: "./src/Assets/hamburguesa_spicy_pulled_pork_1673960044ZD23BB.jpeg"
+        imgUrl: spicyPulledPork
     },
     {
         nombre: "American Style Pork",
         detalle: "Jugoso Pulled Pork bañado en queso cheddar fundido, crujiente tocino, pepinillos dulces y ketchup heinz. Incluye una salsa a elección y una porción de papas fritas crinkle! ",
         precio: "5500",
-        imgUrl: "./src/Assets/hamburguesa_american_style_pork_1673959958DGfHlq.jpeg"
+        imgUrl: americanStylePork
     },
     {
         nombre: "Buffalo Chicken",
         detalle: "Jugoso pollo crispy bañado en queso cheddar fundido, cremoso coleslaw, pepinillos dulces y nuestra Secret Sauce. Incluye una salsa a elección y una porción de papas fritas crinkle!",
         precio: "5500",
-        imgUrl: "./src/Assets/buffalo_chicken_16655253277z965V.jpg"
+        imgUrl: buffaloChicken
     },
     {
         nombre: "Spicy Buffalo Chicken",
         detalle: "Jugoso pollo crispy bañado en salsa buffalo (picor medio/alto) , cremoso coleslaw, pepinillos dulces y nuestra ali oli. Incluye una salsa a elección y una porción de papas fritas crinkle!",
         precio: "5500",
-        imgUrl: "./src/Assets/spicy_buffalo_chicken_1665528552nAEXPA.jpg"
+        imgUrl: spicyBuffaloChicken
     },
     {
         nombre: "American Style Chicken",
         detalle: "Jugoso pollo crispy bañado en queso cheddar fundido, crujiente tocino, pepinillos dulces y ketchup heinz. Incluye una salsa a elección y una porción de papas fritas crinkle!",
         precio: "5500",
-        imgUrl: "./src/Assets/american_style_chicken_166552695897Ngbm.jpg"
+        imgUrl: americanStyleChicken
     },
 
 ]
